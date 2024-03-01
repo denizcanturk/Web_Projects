670c8d2a7aa3920e3f7fd90f14b0c43e8adc1ec6
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-03-01 12:22:29 +0300

    Yeni Düzenlemeler

---
+++ pages/denem.py
@@ -0,0 +1,112 @@
+def my_function():
+    ' ' 'Demonstrates triple double quotes docstrings and does nothing really' ' '
+  
+    return None
+ 
+print("Using __doc__:")
+print(my_function.__doc__)
+ 
+print("Using help:")
+help(my_function)
+"""sayi1 = int(input("Sayi 1 Giriniz : "))
+sayi2 = int(input("Sayi 2 Giriniz : "))
+sonuc = sayi1 * sayi2
+print(sonuc)
+
+
+
+d = 0  #Gecen gun sayisi
+
+elapsedTimeInDays = 0       #Camel Case
+
+elapsed_time_in_days = 0    #Snake Case
+
+ElapsedTimeInDays = 0       #Pascal Case
+
+
+
+
+
+
+a = 1
+print(a)
+a += 2
+
+print(a)
+print(a+7)
+
+
+b = 2.0
+print(b)
+
+print(2.5 + b)
+
+
+
+
+
+
+
+
+print(5/7)
+
+
+
+
+
+
+print(999_999_999_999)
+print(0b0000_0000_0000_0000_0000_0000_1110_1000_1101_0100_1010_0101_0000_1111_1111_1111)
+print(0o16432451207777)
+print(0xE8D4A50FFF)
+
+
+print(bin(999999999999))
+print(oct(999999999999))
+print(hex(999999999999)[2:])
+
+print(111_222_222)
+
+print(0.00000000000000000000001)
+
+
+import sys
+print(sys.maxsize)
+print((2**63)-1)
+
+for i in range(7):
+    print(i)
+    
+
+print(True > False)
+print(True < False)
+
+
+print(-6//4.0)
+a = 4
+b = 534
+
+def onk(a,b):
+    print(a)
+    print(b)
+
+onk(b,a)
+
+
+DENIZ=0
+dENIZ=1
+print(DENIZ)
+print(dENIZ)
+
+isim = input("Lütfen isminizi giriniz : ")
+soyad = input("Soyadınız : ")
+print(f"Adınız {isim} {soyad}, tanıştığımıza memnun oldum... ")
+print(type(isim))
+a = input()
+print(a)
+
+import math
+
+print(math.pi)
+
+"""
---
+++ pages/tekrar.py
@@ -0,0 +1,12 @@
+#While Döngüsü pratiği
+
+sayac = 1
+sayi = int(input("Sayı Giriniz : "))
+
+toplam = 0
+
+while sayac <= sayi:
+    sayac +=1
+    toplam = toplam + sayac
+
+print("Toplam Değeri ", toplam)
